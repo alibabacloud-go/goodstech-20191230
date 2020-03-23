f964ed9cd6cf1695d31a449451da0bbea78bea2f
wenzuochao <wb-wzc505509@alibaba-inc.com>
2020-03-23 11:01:32 +0800

    update product

--- client/client.go
+++ client/client.go
@@ -132,6 +132,214 @@ func (s *Config) SetOpenPlatformEndpoint(v string) *Config {
 	return s
 }
 
+type RecognizeFurnitureAttributeRequest struct {
+	ImageURL *string `json:"ImageURL" xml:"ImageURL" require:"true"`
+}
+
+func (s RecognizeFurnitureAttributeRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s RecognizeFurnitureAttributeRequest) GoString() string {
+	return s.String()
+}
+
+func (s *RecognizeFurnitureAttributeRequest) SetImageURL(v string) *RecognizeFurnitureAttributeRequest {
+	s.ImageURL = &v
+	return s
+}
+
+type RecognizeFurnitureAttributeResponse struct {
+	RequestId *string                                  `json:"RequestId" xml:"RequestId" require:"true"`
+	Data      *RecognizeFurnitureAttributeResponseData `json:"Data" xml:"Data" require:"true" type:"Struct"`
+}
+
+func (s RecognizeFurnitureAttributeResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s RecognizeFurnitureAttributeResponse) GoString() string {
+	return s.String()
+}
+
+func (s *RecognizeFurnitureAttributeResponse) SetRequestId(v string) *RecognizeFurnitureAttributeResponse {
+	s.RequestId = &v
+	return s
+}
+
+func (s *RecognizeFurnitureAttributeResponse) SetData(v *RecognizeFurnitureAttributeResponseData) *RecognizeFurnitureAttributeResponse {
+	s.Data = v
+	return s
+}
+
+type RecognizeFurnitureAttributeResponseData struct {
+	PredStyleId     *string  `json:"PredStyleId" xml:"PredStyleId" require:"true"`
+	PredStyle       *string  `json:"PredStyle" xml:"PredStyle" require:"true"`
+	PredProbability *float32 `json:"PredProbability" xml:"PredProbability" require:"true"`
+}
+
+func (s RecognizeFurnitureAttributeResponseData) String() string {
+	return tea.Prettify(s)
+}
+
+func (s RecognizeFurnitureAttributeResponseData) GoString() string {
+	return s.String()
+}
+
+func (s *RecognizeFurnitureAttributeResponseData) SetPredStyleId(v string) *RecognizeFurnitureAttributeResponseData {
+	s.PredStyleId = &v
+	return s
+}
+
+func (s *RecognizeFurnitureAttributeResponseData) SetPredStyle(v string) *RecognizeFurnitureAttributeResponseData {
+	s.PredStyle = &v
+	return s
+}
+
+func (s *RecognizeFurnitureAttributeResponseData) SetPredProbability(v float32) *RecognizeFurnitureAttributeResponseData {
+	s.PredProbability = &v
+	return s
+}
+
+type RecognizeFurnitureAttributeAdvanceRequest struct {
+	ImageURLObject io.Reader `json:"ImageURLObject" xml:"ImageURLObject" require:"true"`
+}
+
+func (s RecognizeFurnitureAttributeAdvanceRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s RecognizeFurnitureAttributeAdvanceRequest) GoString() string {
+	return s.String()
+}
+
+func (s *RecognizeFurnitureAttributeAdvanceRequest) SetImageURLObject(v io.Reader) *RecognizeFurnitureAttributeAdvanceRequest {
+	s.ImageURLObject = v
+	return s
+}
+
+type RecognizeFurnitureSpuRequest struct {
+	ImageURL *string  `json:"ImageURL" xml:"ImageURL" require:"true"`
+	XLength  *float32 `json:"XLength" xml:"XLength" require:"true"`
+	YLength  *float32 `json:"YLength" xml:"YLength" require:"true"`
+	ZLength  *float32 `json:"ZLength" xml:"ZLength" require:"true"`
+}
+
+func (s RecognizeFurnitureSpuRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s RecognizeFurnitureSpuRequest) GoString() string {
+	return s.String()
+}
+
+func (s *RecognizeFurnitureSpuRequest) SetImageURL(v string) *RecognizeFurnitureSpuRequest {
+	s.ImageURL = &v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuRequest) SetXLength(v float32) *RecognizeFurnitureSpuRequest {
+	s.XLength = &v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuRequest) SetYLength(v float32) *RecognizeFurnitureSpuRequest {
+	s.YLength = &v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuRequest) SetZLength(v float32) *RecognizeFurnitureSpuRequest {
+	s.ZLength = &v
+	return s
+}
+
+type RecognizeFurnitureSpuResponse struct {
+	RequestId *string                            `json:"RequestId" xml:"RequestId" require:"true"`
+	Data      *RecognizeFurnitureSpuResponseData `json:"Data" xml:"Data" require:"true" type:"Struct"`
+}
+
+func (s RecognizeFurnitureSpuResponse) String() string {
+	return tea.Prettify(s)
+}
+
+func (s RecognizeFurnitureSpuResponse) GoString() string {
+	return s.String()
+}
+
+func (s *RecognizeFurnitureSpuResponse) SetRequestId(v string) *RecognizeFurnitureSpuResponse {
+	s.RequestId = &v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuResponse) SetData(v *RecognizeFurnitureSpuResponseData) *RecognizeFurnitureSpuResponse {
+	s.Data = v
+	return s
+}
+
+type RecognizeFurnitureSpuResponseData struct {
+	PredCateId      *string  `json:"PredCateId" xml:"PredCateId" require:"true"`
+	PredCate        *string  `json:"PredCate" xml:"PredCate" require:"true"`
+	PredProbability *float32 `json:"PredProbability" xml:"PredProbability" require:"true"`
+}
+
+func (s RecognizeFurnitureSpuResponseData) String() string {
+	return tea.Prettify(s)
+}
+
+func (s RecognizeFurnitureSpuResponseData) GoString() string {
+	return s.String()
+}
+
+func (s *RecognizeFurnitureSpuResponseData) SetPredCateId(v string) *RecognizeFurnitureSpuResponseData {
+	s.PredCateId = &v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuResponseData) SetPredCate(v string) *RecognizeFurnitureSpuResponseData {
+	s.PredCate = &v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuResponseData) SetPredProbability(v float32) *RecognizeFurnitureSpuResponseData {
+	s.PredProbability = &v
+	return s
+}
+
+type RecognizeFurnitureSpuAdvanceRequest struct {
+	ImageURLObject io.Reader `json:"ImageURLObject" xml:"ImageURLObject" require:"true"`
+	XLength        *float32  `json:"XLength" xml:"XLength" require:"true"`
+	YLength        *float32  `json:"YLength" xml:"YLength" require:"true"`
+	ZLength        *float32  `json:"ZLength" xml:"ZLength" require:"true"`
+}
+
+func (s RecognizeFurnitureSpuAdvanceRequest) String() string {
+	return tea.Prettify(s)
+}
+
+func (s RecognizeFurnitureSpuAdvanceRequest) GoString() string {
+	return s.String()
+}
+
+func (s *RecognizeFurnitureSpuAdvanceRequest) SetImageURLObject(v io.Reader) *RecognizeFurnitureSpuAdvanceRequest {
+	s.ImageURLObject = v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuAdvanceRequest) SetXLength(v float32) *RecognizeFurnitureSpuAdvanceRequest {
+	s.XLength = &v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuAdvanceRequest) SetYLength(v float32) *RecognizeFurnitureSpuAdvanceRequest {
+	s.YLength = &v
+	return s
+}
+
+func (s *RecognizeFurnitureSpuAdvanceRequest) SetZLength(v float32) *RecognizeFurnitureSpuAdvanceRequest {
+	s.ZLength = &v
+	return s
+}
+
 type ClassifyCommodityRequest struct {
 	ImageURL *string `json:"ImageURL" xml:"ImageURL" require:"true"`
 }
@@ -255,11 +463,11 @@ type Client struct {
 
 func NewClient(config *Config) (*Client, error) {
 	client := new(Client)
-	err := client.init(config)
+	err := client.Init(config)
 	return client, err
 }
 
-func (client *Client) init(config *Config) (_err error) {
+func (client *Client) Init(config *Config) (_err error) {
 	if util.IsUnset(tea.ToMap(config)) {
 		_err = tea.NewSDKError(map[string]interface{}{
 			"name":    "ParameterMissing",
@@ -316,7 +524,7 @@ func (client *Client) init(config *Config) (_err error) {
 	return nil
 }
 
-func (client *Client) _request(action string, protocol string, method string, request map[string]interface{}, runtime *util.RuntimeOptions) (_result map[string]interface{}, _err error) {
+func (client *Client) _request(action string, protocol string, method string, authType string, query map[string]interface{}, body map[string]interface{}, runtime *util.RuntimeOptions) (_result map[string]interface{}, _err error) {
 	_err = tea.Validate(runtime)
 	if _err != nil {
 		return nil, _err
@@ -354,32 +562,40 @@ func (client *Client) _request(action string, protocol string, method string, re
 			request_.Protocol = util.DefaultString(client.Protocol, protocol)
 			request_.Method = method
 			request_.Pathname = "/"
-			accessKeyId, _err := client.GetAccessKeyId()
-			if _err != nil {
-				return nil, _err
-			}
-
-			accessKeySecret, _err := client.GetAccessKeySecret()
-			if _err != nil {
-				return nil, _err
-			}
-
 			request_.Query = rpcutil.Query(tea.ToMap(map[string]interface{}{
-				"Action":           action,
-				"Format":           "json",
-				"RegionId":         client.RegionId,
-				"Timestamp":        rpcutil.GetTimestamp(),
-				"Version":          "2019-12-30",
-				"SignatureMethod":  "HMAC-SHA1",
-				"SignatureVersion": "1.0",
-				"SignatureNonce":   util.GetNonce(),
-				"AccessKeyId":      accessKeyId,
-			}, request))
+				"Action":         action,
+				"Format":         "json",
+				"RegionId":       client.RegionId,
+				"Timestamp":      rpcutil.GetTimestamp(),
+				"Version":        "2019-12-30",
+				"SignatureNonce": util.GetNonce(),
+			}, query))
+			if !util.IsUnset(body) {
+				tmp := util.AnyifyMapValue(rpcutil.Query(body))
+				request_.Body = tea.ToReader(util.ToFormString(tmp))
+			}
+
 			request_.Headers = map[string]string{
 				"host":       rpcutil.GetHost("goodstech", client.RegionId, client.Endpoint),
 				"user-agent": client.GetUserAgent(),
 			}
-			request_.Query["Signature"] = rpcutil.GetSignature(request_, accessKeySecret)
+			if !util.EqualString(authType, "Anonymous") {
+				accessKeyId, _err := client.GetAccessKeyId()
+				if _err != nil {
+					return nil, _err
+				}
+
+				accessKeySecret, _err := client.GetAccessKeySecret()
+				if _err != nil {
+					return nil, _err
+				}
+
+				request_.Query["SignatureMethod"] = "HMAC-SHA1"
+				request_.Query["SignatureVersion"] = "1.0"
+				request_.Query["AccessKeyId"] = accessKeyId
+				request_.Query["Signature"] = rpcutil.GetSignature(request_, accessKeySecret)
+			}
+
 			response_, _err := tea.DoRequest(request_, _runtime)
 			if _err != nil {
 				return nil, _err
@@ -389,17 +605,17 @@ func (client *Client) _request(action string, protocol string, method string, re
 				return nil, _err
 			}
 
-			body := util.AssertAsMap(obj)
+			res := util.AssertAsMap(obj)
 			if util.Is4xx(response_.StatusCode) || util.Is5xx(response_.StatusCode) {
 				_err = tea.NewSDKError(map[string]interface{}{
-					"message": body["Message"],
-					"data":    body,
-					"code":    body["Code"],
+					"message": res["Message"],
+					"data":    res,
+					"code":    res["Code"],
 				})
 				return nil, _err
 			}
 
-			_result = body
+			_result = res
 			return _result, _err
 		}()
 		if !tea.Retryable(_err) {
@@ -410,9 +626,9 @@ func (client *Client) _request(action string, protocol string, method string, re
 	return _resp, _err
 }
 
-func (client *Client) ClassifyCommodity(request *ClassifyCommodityRequest, runtime *util.RuntimeOptions) (_result *ClassifyCommodityResponse, _err error) {
-	_result = &ClassifyCommodityResponse{}
-	_body, _err := client._request("ClassifyCommodity", "HTTPS", "GET", tea.ToMap(request), runtime)
+func (client *Client) RecognizeFurnitureAttribute(request *RecognizeFurnitureAttributeRequest, runtime *util.RuntimeOptions) (_result *RecognizeFurnitureAttributeResponse, _err error) {
+	_result = &RecognizeFurnitureAttributeResponse{}
+	_body, _err := client._request("RecognizeFurnitureAttribute", "HTTPS", "POST", "AK", nil, tea.ToMap(request), runtime)
 	if _err != nil {
 		return nil, _err
 	}
@@ -420,7 +636,8 @@ func (client *Client) ClassifyCommodity(request *ClassifyCommodityRequest, runti
 	return _result, _err
 }
 
-func (client *Client) ClassifyCommodityAdvance(request *ClassifyCommodityAdvanceRequest, runtime *util.RuntimeOptions) (_result *ClassifyCommodityResponse, _err error) {
+func (client *Client) RecognizeFurnitureAttributeAdvance(request *RecognizeFurnitureAttributeAdvanceRequest, runtime *util.RuntimeOptions) (_result *RecognizeFurnitureAttributeResponse, _err error) {
+	// Step 0: init client
 	accessKeyId, _err := client.Credential.GetAccessKeyId()
 	if _err != nil {
 		return nil, _err
@@ -453,6 +670,7 @@ func (client *Client) ClassifyCommodityAdvance(request *ClassifyCommodityAdvance
 		return nil, _err
 	}
 
+	// Step 1: request OSS api to upload file
 	ossConfig := &oss.Config{
 		AccessKeyId:     authResponse.AccessKeyId,
 		AccessKeySecret: tea.String(accessKeySecret),
@@ -489,6 +707,195 @@ func (client *Client) ClassifyCommodityAdvance(request *ClassifyCommodityAdvance
 	if _err != nil {
 		return
 	}
+	// Step 2: request final api
+	recognizeFurnitureAttributereq := &RecognizeFurnitureAttributeRequest{}
+	rpcutil.Convert(request, recognizeFurnitureAttributereq)
+	recognizeFurnitureAttributereq.ImageURL = tea.String("http://" + tea.StringValue(authResponse.Bucket) + "." + tea.StringValue(authResponse.Endpoint) + "/" + tea.StringValue(authResponse.ObjectKey))
+	recognizeFurnitureAttributeResp, _err := client.RecognizeFurnitureAttribute(recognizeFurnitureAttributereq, runtime)
+	if _err != nil {
+		return nil, _err
+	}
+
+	_result = recognizeFurnitureAttributeResp
+	return _result, _err
+}
+
+func (client *Client) RecognizeFurnitureSpu(request *RecognizeFurnitureSpuRequest, runtime *util.RuntimeOptions) (_result *RecognizeFurnitureSpuResponse, _err error) {
+	_result = &RecognizeFurnitureSpuResponse{}
+	_body, _err := client._request("RecognizeFurnitureSpu", "HTTPS", "POST", "AK", nil, tea.ToMap(request), runtime)
+	if _err != nil {
+		return nil, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) RecognizeFurnitureSpuAdvance(request *RecognizeFurnitureSpuAdvanceRequest, runtime *util.RuntimeOptions) (_result *RecognizeFurnitureSpuResponse, _err error) {
+	// Step 0: init client
+	accessKeyId, _err := client.Credential.GetAccessKeyId()
+	if _err != nil {
+		return nil, _err
+	}
+
+	accessKeySecret, _err := client.Credential.GetAccessKeySecret()
+	if _err != nil {
+		return nil, _err
+	}
+
+	authConfig := &openplatform.Config{
+		AccessKeyId:     tea.String(accessKeyId),
+		AccessKeySecret: tea.String(accessKeySecret),
+		Type:            tea.String("access_key"),
+		Endpoint:        tea.String("openplatform.aliyuncs.com"),
+		Protocol:        tea.String(client.Protocol),
+		RegionId:        tea.String(client.RegionId),
+	}
+	authClient, _err := openplatform.NewClient(authConfig)
+	if _err != nil {
+		return nil, _err
+	}
+
+	authRequest := &openplatform.AuthorizeFileUploadRequest{
+		Product:  tea.String("goodstech"),
+		RegionId: tea.String(client.RegionId),
+	}
+	authResponse, _err := authClient.AuthorizeFileUpload(authRequest, runtime)
+	if _err != nil {
+		return nil, _err
+	}
+
+	// Step 1: request OSS api to upload file
+	ossConfig := &oss.Config{
+		AccessKeyId:     authResponse.AccessKeyId,
+		AccessKeySecret: tea.String(accessKeySecret),
+		Type:            tea.String("access_key"),
+		Endpoint:        tea.String(rpcutil.GetEndpoint(tea.StringValue(authResponse.Endpoint), tea.BoolValue(authResponse.UseAccelerate), client.EndpointType)),
+		Protocol:        tea.String(client.Protocol),
+		RegionId:        tea.String(client.RegionId),
+	}
+	ossClient, _err := oss.NewClient(ossConfig)
+	if _err != nil {
+		return nil, _err
+	}
+
+	fileObj := &fileform.FileField{
+		Filename:    authResponse.ObjectKey,
+		Content:     request.ImageURLObject,
+		ContentType: tea.String(""),
+	}
+	ossHeader := &oss.PostObjectRequestHeader{
+		AccessKeyId:         authResponse.AccessKeyId,
+		Policy:              authResponse.EncodedPolicy,
+		Signature:           authResponse.Signature,
+		Key:                 authResponse.ObjectKey,
+		File:                fileObj,
+		SuccessActionStatus: tea.String("201"),
+	}
+	uploadRequest := &oss.PostObjectRequest{
+		BucketName: authResponse.Bucket,
+		Header:     ossHeader,
+	}
+	ossRuntime := &ossutil.RuntimeOptions{}
+	rpcutil.Convert(runtime, ossRuntime)
+	_, _err = ossClient.PostObject(uploadRequest, ossRuntime)
+	if _err != nil {
+		return
+	}
+	// Step 2: request final api
+	recognizeFurnitureSpureq := &RecognizeFurnitureSpuRequest{}
+	rpcutil.Convert(request, recognizeFurnitureSpureq)
+	recognizeFurnitureSpureq.ImageURL = tea.String("http://" + tea.StringValue(authResponse.Bucket) + "." + tea.StringValue(authResponse.Endpoint) + "/" + tea.StringValue(authResponse.ObjectKey))
+	recognizeFurnitureSpuResp, _err := client.RecognizeFurnitureSpu(recognizeFurnitureSpureq, runtime)
+	if _err != nil {
+		return nil, _err
+	}
+
+	_result = recognizeFurnitureSpuResp
+	return _result, _err
+}
+
+func (client *Client) ClassifyCommodity(request *ClassifyCommodityRequest, runtime *util.RuntimeOptions) (_result *ClassifyCommodityResponse, _err error) {
+	_result = &ClassifyCommodityResponse{}
+	_body, _err := client._request("ClassifyCommodity", "HTTPS", "GET", "AK", tea.ToMap(request), nil, runtime)
+	if _err != nil {
+		return nil, _err
+	}
+	_err = tea.Convert(_body, &_result)
+	return _result, _err
+}
+
+func (client *Client) ClassifyCommodityAdvance(request *ClassifyCommodityAdvanceRequest, runtime *util.RuntimeOptions) (_result *ClassifyCommodityResponse, _err error) {
+	// Step 0: init client
+	accessKeyId, _err := client.Credential.GetAccessKeyId()
+	if _err != nil {
+		return nil, _err
+	}
+
+	accessKeySecret, _err := client.Credential.GetAccessKeySecret()
+	if _err != nil {
+		return nil, _err
+	}
+
+	authConfig := &openplatform.Config{
+		AccessKeyId:     tea.String(accessKeyId),
+		AccessKeySecret: tea.String(accessKeySecret),
+		Type:            tea.String("access_key"),
+		Endpoint:        tea.String("openplatform.aliyuncs.com"),
+		Protocol:        tea.String(client.Protocol),
+		RegionId:        tea.String(client.RegionId),
+	}
+	authClient, _err := openplatform.NewClient(authConfig)
+	if _err != nil {
+		return nil, _err
+	}
+
+	authRequest := &openplatform.AuthorizeFileUploadRequest{
+		Product:  tea.String("goodstech"),
+		RegionId: tea.String(client.RegionId),
+	}
+	authResponse, _err := authClient.AuthorizeFileUpload(authRequest, runtime)
+	if _err != nil {
+		return nil, _err
+	}
+
+	// Step 1: request OSS api to upload file
+	ossConfig := &oss.Config{
+		AccessKeyId:     authResponse.AccessKeyId,
+		AccessKeySecret: tea.String(accessKeySecret),
+		Type:            tea.String("access_key"),
+		Endpoint:        tea.String(rpcutil.GetEndpoint(tea.StringValue(authResponse.Endpoint), tea.BoolValue(authResponse.UseAccelerate), client.EndpointType)),
+		Protocol:        tea.String(client.Protocol),
+		RegionId:        tea.String(client.RegionId),
+	}
+	ossClient, _err := oss.NewClient(ossConfig)
+	if _err != nil {
+		return nil, _err
+	}
+
+	fileObj := &fileform.FileField{
+		Filename:    authResponse.ObjectKey,
+		Content:     request.ImageURLObject,
+		ContentType: tea.String(""),
+	}
+	ossHeader := &oss.PostObjectRequestHeader{
+		AccessKeyId:         authResponse.AccessKeyId,
+		Policy:              authResponse.EncodedPolicy,
+		Signature:           authResponse.Signature,
+		Key:                 authResponse.ObjectKey,
+		File:                fileObj,
+		SuccessActionStatus: tea.String("201"),
+	}
+	uploadRequest := &oss.PostObjectRequest{
+		BucketName: authResponse.Bucket,
+		Header:     ossHeader,
+	}
+	ossRuntime := &ossutil.RuntimeOptions{}
+	rpcutil.Convert(runtime, ossRuntime)
+	_, _err = ossClient.PostObject(uploadRequest, ossRuntime)
+	if _err != nil {
+		return
+	}
+	// Step 2: request final api
 	classifyCommodityreq := &ClassifyCommodityRequest{}
 	rpcutil.Convert(request, classifyCommodityreq)
 	classifyCommodityreq.ImageURL = tea.String("http://" + tea.StringValue(authResponse.Bucket) + "." + tea.StringValue(authResponse.Endpoint) + "/" + tea.StringValue(authResponse.ObjectKey))
